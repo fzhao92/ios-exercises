b94ff5186ac89790af4d809ed4f94930c18a7d9b
Forrest Zhao <fozhao92@gmail.com>
2016-02-03 22:54:05 -0800

    Completed assignment 11

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -11,9 +11,25 @@
 @implementation MarysAppleHandler
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
-    NSString *itemToReturn;
 
+    NSString *itemToReturn;
     /* WORK HERE */
+    if (dollars <= 4) {
+        itemToReturn = @"get out of my store";
+    }
+    if (dollars == 5) {
+        itemToReturn = @"have some gum";
+    }
+    if (dollars == 6) {
+        itemToReturn = @"have an apple";
+    }
+    if (dollars == 1000) {
+        itemToReturn = @"have an Apple computer";
+    }
+    if (dollars == 1000000000) {
+        itemToReturn = @"have The Big Apple";
+    }
+    
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
@@ -24,11 +40,7 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
-    
-    return cost;
+    return self.getsDiscount ? cost*.75: cost;
 }
 
 @end
