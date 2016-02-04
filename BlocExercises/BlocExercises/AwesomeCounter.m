2d1714f083a7e6a2cab9d43a85891cdd11af1602
Forrest Zhao <fozhao92@gmail.com>
2016-02-04 00:00:33 -0800

    Completed assignment 12

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,23 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSMutableString *ret = [NSMutableString stringWithString: @""];
+    if (otherNumber > number) {
+        while(number <= otherNumber){
+            [ret appendString: [NSString stringWithFormat: @"%ld", number]];
+            number++;
+        }
+    }
+    else if(otherNumber < number){
+        while(otherNumber <= number){
+            [ret appendString: [NSString stringWithFormat: @"%ld", otherNumber]];
+            otherNumber++;
+        }
+    }
+    else{
+        [ret appendString: [NSString stringWithFormat: @"%ld", number]];
+    }
+    return ret;
 }
 
 @end
